f1f4b47c2b96f54f30eac7fa68dacdb62b049625
alitebourbiwsb <ali.tebourbi@student.wsb.edu.pl>
2026-01-17 19:35:30 +0000

    TASK2: S2_FOR_01 - fizzbuzz with for loop

---
+++ js/stage2/S2_FOR_01.js
@@ -0,0 +1,16 @@
+// S2_FOR_01 — FizzBuzz+
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 15 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+// Test
+fizzBuzz(20);
