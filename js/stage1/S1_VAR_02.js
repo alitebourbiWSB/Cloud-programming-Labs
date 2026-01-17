4f32be52819bf4270bcf46bca5ad2c8a920c4b7d
alitebourbiWSB <ali.tebourbi@student.wsb.edu.pl>
2026-01-17 16:50:13 +0100

    TASK1: S1_VAR_02 - let vs var block scope demo

--- js/stage1/S1_VAR_02.js
+++ js/stage1/S1_VAR_02.js
@@ -1,4 +1,38 @@
-// Stage 1 - Variable example 02
-let counter = 0;
-counter += 1;
-console.log(counter);
+// S1_VAR_02 - Block scope check
+
+try {
+
+  {
+
+    let x = 10;
+
+  }
+
+  console.log(x);
+
+} catch (e) {
+
+  console.log("let is block-scoped and not accessible outside");
+
+}
+
+try {
+
+  {
+
+    var y = 20;
+
+  }
+
+  console.log("var value outside block:", y);
+
+} catch (e) {
+
+  console.log("This will not happen for var");
+
+}
+
+// Explanation:
+
+// let is block-scoped, var is function-scoped
+ 
